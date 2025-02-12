0034a03f7b767ad9a67824df19439fd88515b970
assistantmam29 <manishasinghch95@gmail.com>
2025-02-12 11:43:12 +0530

    Add files via upload

---
+++ Mohan.java
@@ -0,0 +1,33 @@
+class B{
+	private int mac;
+	 private int camar;
+	void fun1(){
+		 mac=10;
+		 camar=20;
+		//System.out.println(mac);
+		//System.out.println(camar);
+		
+		}
+
+	 void fun2(){
+		 //mac=2;
+		 //camar=5;
+		System.out.println(mac);
+		System.out.println(camar);
+
+}}
+		
+
+
+class Mohan{
+   	public static void main(String args []){
+	System.out.println(" Radhe Radhe");
+	B b1=new B();
+	 b1.fun2();
+	b1.fun1();
+	b1.fun2();
+	
+	
+	
+	
+}}
